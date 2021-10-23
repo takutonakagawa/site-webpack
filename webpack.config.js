82bac5935447a9198ad40d94d4272bbed5b03402
tackt21 <t-nakagawa@willpride.co.jp>
2021-10-23 18:28:57 +0900

    'file-loader'

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpg)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "assets/images/[name].[ext]",
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
